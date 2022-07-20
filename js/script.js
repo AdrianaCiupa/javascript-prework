f88c8d7c00d6bba4e25116aeb826e226e013a211
Adriana Ciupa <adriana.ciupa94@gmail.com>
2022-07-20 22:57:43 +0200

    correct code

--- js/script.js
+++ js/script.js
@@ -1,17 +1,10 @@
-var buttonPaper, buttonRock, buttonScissors;
 
-/**
- * Describe this function...
- */
-function buttonClicked(buttonPaper) {
-  clearMessages();
-  console.log(buttonPaper + ' został kliknięty');
-}
-buttonRock = document.getElementById('button-rock');
+
+let buttonRock = document.getElementById('button-rock');
 buttonRock.addEventListener('click', function(){ buttonClicked('kamień'); });
-buttonPaper = document.getElementById('button-paper');
+let buttonPaper = document.getElementById('button-paper');
 buttonPaper.addEventListener('click', function(){ buttonClicked('papier'); });
-buttonScissors = document.getElementById('button-scissors');
+let buttonScissors = document.getElementById('button-scissors');
 buttonScissors.addEventListener('click', function(){ buttonClicked('nożyce'); });
 function printMessage(msg){
 	const div = document.createElement('div');
@@ -23,12 +16,7 @@ function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
 }
 
-let argMoveId, argPlayerMove, argComputerMove, computerMove, playerMove, randomNumber, playerInput;
-
-/**
- * Describe this function...
- */
-function getMoveName(argMoveId) {
+const getMoveName = function(argMoveId) {
   console.log('wywołano funkcję getMoveName z argumentem: ' + argMoveId);
   if (argMoveId == 1) {
     return 'kamień';
@@ -42,10 +30,7 @@ function getMoveName(argMoveId) {
   }
 }
 
-/**
- * Describe this function...
- */
-function displayResult(argPlayerMove, argComputerMove) {
+const displayResult = function(argPlayerMove, argComputerMove) {
   console.log('wywołano funkcję displayResults z argumentami: ' + argPlayerMove + ', ' + argComputerMove);
   if (argPlayerMove == 'papier' && argComputerMove == 'kamień') {
     printMessage('Wygrywasz!');
@@ -68,6 +53,4 @@ randomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to: ' + randomNumber);
 computerMove = getMoveName(randomNumber);
 console.log('ruch komputera to: ' + computerMove);
-displayResult(playerMove, computerMove);
-
 
